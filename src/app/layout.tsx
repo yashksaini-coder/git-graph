6836b7ae1992f9ec2ee5f87919db2c4d3133d8c7
yashksaini-coder <ys3853428@gmail.com>
2025-03-06 23:51:05 +0530

    Replace Geist font with Ubuntu Mono in layout; simplify body class management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Ubuntu_Sans_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const UbuntuMono = Ubuntu_Sans_Mono({
+  variable: "--font-ubuntu-mono",
   subsets: ["latin"],
 });
 
@@ -23,10 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={UbuntuMono.className}>
+      <body>
         {children}
       </body>
     </html>
