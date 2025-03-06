6832b303004d04a34185914799e86af105c5cc34
yashksaini-coder <ys3853428@gmail.com>
2025-03-07 00:40:30 +0530

    Add Footer component with copyright information and link to author

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+export const Footer = () => {
+  return (
+    <footer className="absolute bottom-2 w-full text-center text-sm text-gray-400" suppressHydrationWarning={true}>
+      © {new Date().getFullYear()} By{' '}
+      <Button variant="link" className="p-0 text-white">
+        <a href="https://github.com/yashksaini-coder" className='hover:bg-violet-600 hover:text-black rounded-none'>Yash K. Saini</a>
+      </Button>
+    </footer>
+  );
+};
