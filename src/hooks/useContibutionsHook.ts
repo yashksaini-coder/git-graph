6438b77de9a4b07d9f097f387952029f8d987ccf
Kush Chaudhary <kushchaudharyog@gmail.com>
2025-03-09 14:25:03 +0530

    Done

---
+++ src/hooks/useContibutionsHook.ts
@@ -0,0 +1,31 @@
+import { getContributions } from "@/app/api";
+import { ContributionCalendar } from "@/utils/types";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+export const useContibutions = (username: string) => {
+  const [contributionData, setContributionData] =
+    useState<ContributionCalendar | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchContributions = async () => {
+    try {
+      const contributions = await getContributions(username);
+      if (!contributions) {
+        toast.error("No contributions found for this user");
+      } else {
+        setContributionData(contributions);
+      }
+    } catch {
+      toast.error("Failed to fetch contributions");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchContributions();
+  }, [username]);
+
+  return { contributionData, isLoading };
+};
