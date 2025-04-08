169b9a414a27a4183cfb457a1505e2e88cd37cb7
Sibam Paul <158052549+Sibam-Paul@users.noreply.github.com>
2025-04-08 23:06:43 +0530

    Add PR, issue, Repo icons
    Removed showProfile funtionality
    
    Co-Authored-By: 𝕪𝕒𝕤𝕙𝕜𝕤𝕒𝕚𝕟𝕚-𝕔𝕠𝕕𝕖𝕣 <115717039+yashksaini-coder@users.noreply.github.com>
    Co-Authored-By: Sibam Paul <158052549+sibam-paul@users.noreply.github.com>

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -5,23 +5,27 @@ import { motion, AnimatePresence } from "framer-motion";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card } from "@/components/ui/card";
 // import { Skeleton } from "@/components/ui/skeleton";
-import { Users, GitCommit } from "lucide-react";
+import { Users } from "lucide-react";
 import { getProfile } from "@/app/api";
 import { UserProfileType } from "@/utils/types";
 import { toast } from "sonner";
+import {
+  RepoIcon,
+  IssueOpenedIcon,
+  GitPullRequestIcon,
+} from "@primer/octicons-react";
 
 interface UserInfoProps {
   username?: string;
-  showProfile: boolean;
 }
 
-export default function UserInfo({ username, showProfile }: UserInfoProps) {
+export default function UserInfo({ username }: UserInfoProps) {
   const [profile, setProfile] = useState<UserProfileType | null>(null);
   // const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchUserProfile = async () => {
-      if (!showProfile) return;
+      // if (!showProfile) return;
       // setIsLoading(true);
 
       try {
@@ -40,11 +44,11 @@ export default function UserInfo({ username, showProfile }: UserInfoProps) {
     };
 
     fetchUserProfile();
-  }, [username, showProfile]);
+  }, [username]);
 
   return (
     <AnimatePresence>
-      {showProfile && (
+      {username && (
         <motion.div
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
@@ -68,15 +72,44 @@ export default function UserInfo({ username, showProfile }: UserInfoProps) {
                 </Avatar>
                 <div className="space-y-2 flex-1">
                   <div>
-                    <h1 className="text-2xl font-bold text-white">
-                      {typeof profile.name === "string" ? profile.name : "User"}
-                    </h1>
-                    <p className="text-gray-400">
-                      @
-                      {typeof profile.name === "string"
-                        ? profile.name
-                        : username}
-                    </p>
+                    <div className="flex justify-between">
+                      <h1 className="text-2xl font-bold text-white">
+                        {typeof profile.name === "string"
+                          ? profile.name
+                          : "User"}
+                      </h1>
+                      <div className="flex items-center gap-1 text-gray-400 text-sm">
+                        {/* <GitCommit className="w-4 h-4" /> */}
+                        <RepoIcon size={15} />
+                        <span>
+                          {profile.repositories.totalCount} Repositories
+                        </span>
+                      </div>
+                    </div>
+                    <div className="flex justify-between h-20px">
+                      <p className="text-gray-400">
+                        @
+                        {typeof profile.name === "string"
+                          ? profile.name
+                          : username}
+                      </p>
+                      <div className="flex text-sm text-gray-400 justify-center items-center gap-1 w-20% text-sm">
+                        <div className="flex items-center gap-1 pr-5">
+                          <IssueOpenedIcon size={15} />
+                          <div className="flex item-center gap-1">
+                            {profile.issues.totalCount}
+                            <div>Issues</div>
+                          </div>
+                        </div>
+                        <div className="flex items-center gap-1">
+                          <GitPullRequestIcon size={15} />
+                          <div className="flex flex item-center gap-1">
+                            {profile.pullRequests.totalCount}
+                            <div>Pull Request</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                   <p className="text-gray-200">
                     {typeof profile.bio === "string"
@@ -91,14 +124,6 @@ export default function UserInfo({ username, showProfile }: UserInfoProps) {
                       <span>
                         {profile.following.totalCount}
                         {""} Following
-                      </span>
-                    </div>
-                  </div>
-                  <div className="flex items-center gap-4 text-gray-400">
-                    <div className="flex items-center gap-1">
-                      <GitCommit className="w-4 h-4" />
-                      <span>
-                        {profile.repositories.totalCount} Repositories
                       </span>
                     </div>
                   </div>
